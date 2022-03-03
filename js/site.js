1d5e975183b0f46cf71e7fed90c054ffd3e2e450
zzhang025 <zhangzx45@gmail.com>
2022-03-03 14:07:26 -0800

    AppJsCodes

--- js/site.js
+++ js/site.js
@@ -1,4 +1,38 @@
-//Call hello world
-function Helloworld(){
-    alert("hello world");
+//Get the value from the page
+
+//Controller Function
+function getInput(){
+
+    document.getElementById('alert').classList.add("invisible");
+
+    let inputstring = document.getElementById('inputString').value;
+
+    let revstring = reverseString(inputstring);
+
+    display(revstring);
+}
+
+//Reverse the String
+//logic function
+function reverseString(inputstring){
+
+    let revstring=[];
+
+    // for loop
+
+    for (let index = inputstring.length; index >= 0 ; index--) {
+        revstring += inputstring[index];  
+    }
+    return revstring;
+}
+
+//Display the Message
+//View function
+function display(revstring){
+
+    //make the alert visible
+    document.getElementById('alert').classList.remove("invisible");
+
+    //display the message
+    document.getElementById('msg').innerHTML = `Your String reversed is: ${revstring}`;
 }
